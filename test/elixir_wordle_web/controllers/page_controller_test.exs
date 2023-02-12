9ddbc7edb8b96ce70ac2e5f4412e81de0f19cb1f
IciaCarroBarallobre <iciacarrobarallobre@gmail.com>
2023-02-12 20:33:08 +0100

    delete unuseful test

--- test/elixir_wordle_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirWordleWeb.PageControllerTest do
-  use ElixirWordleWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
