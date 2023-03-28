88ebc16ada4b6bd4efa926d93bf556c7b7d0eb57
IciaCarroBarallobre <iciacarrobarallobre@gmail.com>
2023-03-28 16:21:00 +0200

    Info at wordle_live cause is related to wordle_live

--- lib/elixir_wordle_web/live/wordle_live.ex
+++ lib/elixir_wordle_web/live/wordle_live.ex
@@ -72,6 +72,15 @@ defmodule ElixirWordleWeb.WordleLive do
 
   def render(assigns) do
     ~H"""
+    <button
+      phx-click={show_modal("wordle-rules")}
+      class="mx-auto rounded-md right-0 top-3 z-40  mt-2 sm:mt-1 fixed bg-slate-100 mr-4 sm:mr-8 border border-gray-200"
+    >
+      <Heroicons.question_mark_circle class=" h-5 w-5 md:w-8 md:h-8  text-gray-500" />
+    </button>
+
+    <%= modal_rules(assigns) %>
+
     <p class="bg-slate-100 rounded adjust-content mx-auto text-center w-fit px-2 mt-2 mb-6">
       Play a version of Wordle where <strong> all the words are related to Elixir</strong>.
     </p>
@@ -120,5 +129,42 @@ defmodule ElixirWordleWeb.WordleLive do
   defp fill_guesses([{guess, _feedback} | _t] = guesses, attempts) when attempts > 0 do
     word = for _i <- 1..String.length(guess), do: " ", into: ""
     fill_guesses([{word, nil} | guesses], attempts - 1)
+  end
+
+  def modal_rules(assigns) do
+    ~H"""
+    <.modal id="wordle-rules">
+      <:title>
+        <span class="text-center font-bold text-2xl  text-darkest_purple"> How to play </span>
+      </:title>
+
+      <div class="mt-2 text-sm leading-6 text-zinc-600">
+        <span>
+          In Elixir Wordle, players try to guess an Elixir-related word in a maximun of 6 tries.
+        </span>
+        <br /><br /> To start the game, enter any word: <br /><br />
+        <.live_component
+          module={ElixirWordleWeb.GuessesBoard}
+          id="example-guesses"
+          columns={6}
+          guesses={[{"erlang", [:match, :letter_match, :letter_match, :fail, :fail, :fail]}]}
+        />
+        <br />
+        <ul class=" rounded bg-slate-50">
+          <li>
+            <span class="font-bold"> A </span>, <span class="font-bold"> N </span>,
+            <span class="font-bold"> G </span>
+            aren't in the word at all.
+          </li>
+          <li>
+            <span class="font-bold"> R </span>, <span class="font-bold"> L </span>
+            is in the word but in the wrong tile.
+          </li>
+          <li><span class="font-bold"> E </span> is in the word and in the correct tile.</li>
+        </ul>
+        <br /> So, next try could be <span class="text-darkest_purple font-bold">ELIXIR</span>.
+      </div>
+    </.modal>
+    """
   end
 end
