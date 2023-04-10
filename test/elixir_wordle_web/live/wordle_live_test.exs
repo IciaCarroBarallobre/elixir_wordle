6d8056d0a9c54c37f3d1385e25fa6dfb99fdb4e3
IciaCarroBarallobre <iciacarrobarallobre@gmail.com>
2023-04-10 20:31:39 +0200

    improve test & include results in test

--- test/elixir_wordle_web/live/wordle_live_test.exs
+++ test/elixir_wordle_web/live/wordle_live_test.exs
@@ -7,88 +7,83 @@ defmodule ElixirWordleWeb.WordleLiveTest do
 
   setup :verify_on_exit!
 
-  describe "directly testable scenarios derived from mount" do
+  @generic_data %{
+    answer: "erlang",
+    clue: "A language",
+    description: "Erlang is a general-purpose programming language and runtime environment.
+          Erlang has built-in support for concurrency, distribution and fault tolerance."
+  }
+
+  @max_attempts 6
+
+  describe "mount" do
     test "when the word cannot be obtained, this should be reflected in the view", %{conn: conn} do
       ElixirWordle.MockWordleAPI
       |> expect(:get_word_info, 2, fn -> {:error, "There is an error"} end)
 
       assert {:ok, view, html} = live(conn, "/")
 
-      # Image error msg
+      # When word is not available, set image error and warning at clue
       assert view |> element("#image_error") |> render() =~ "Word not available"
+      assert view |> element("#clue") |> render() =~ "Today&#39;s word is not available"
 
-      # Input Board & Guesses board is no setted
-      html =~ "guesses-board"
-      html =~ "inputs-board"
-
-      # Clue behaviour
-      clue_html = view |> element("#clue") |> render()
-      assert clue_html =~ "Today&#39;s word is not available"
+      # When word is not available, board is not displayed
+      refute html =~ "guesses-board"
+      refute html =~ "inputs-board"
     end
 
-    test "when the answer doesn't have a supported length, the word is considered unavailable in the view",
+    test "when the answer doesn't have a supported length (3..8), the word is considered unavailable in the view",
          %{conn: conn} do
       mock_data = %{answer: "ab", clue: "This is the clue", description: ""}
 
       ElixirWordle.MockWordleAPI
       |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
 
+      # When word is not available, set image error and warning at clue
       assert {:ok, view, html} = live(conn, "/")
-
-      # Image error msg
       assert view |> element("#image_error") |> render() =~ "Word not available"
 
-      # Input Board & Guesses board is no setted
-      html =~ "guesses-board"
-      html =~ "inputs-board"
-
-      # Clue behaviour
       clue_html = view |> element("#clue") |> render()
       assert clue_html =~ "Today&#39;s word is not available"
-      refute clue_html =~ mock_data.clue
+
+      # When word is not available, board is not displayed
+      refute html =~ "guesses-board"
+      refute html =~ "inputs-board"
     end
 
-    test "when the answer has a supported length, both the input board and the clue are set", %{
-      conn: conn
-    } do
-      mock_data = %{answer: "abcdef", clue: "This is the clue", description: ""}
-      columns = String.length(mock_data.answer)
-
-      ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
+    test "when the answer has a supported length, both the input board and the clue are set",
+         %{conn: conn} do
+      ElixirWordle.MockWordleAPI |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
 
       assert {:ok, view, _html} = live(conn, "/")
 
-      # Board image, 6 attempts
-      input_board_html = view |> element("#inputs-board") |> render()
+      # Set the clue & the input board with maximum number attempts
+      assert view |> element("#clue") |> render() =~ @generic_data.clue
 
-      assert String.contains?(input_board_html, [
+      columns = String.length(@generic_data.answer)
+      inputs_html = view |> element("#inputs-board") |> render()
+
+      assert inputs_html
+             |> String.contains?([
                "input-1-1",
                "input-1-#{columns}",
                "input-6-1",
                "input-6-#{columns}"
              ])
-
-      # Clue
-      assert view |> element("#clue") |> render() =~ mock_data.clue
     end
   end
 
-  describe "directly testable scenarios derived from when the guess is submitted and there are more attempts" do
-    test "when the guess is not the answer but has the same length, the guess will be displayed in the view",
+  describe "submit (when there are more attempts)" do
+    test "when the guess is not the answer but has the same length, the guess will be shown at the guess board",
          %{conn: conn} do
-      mock_data = %{answer: "elixirw", clue: "This is the clue", description: ""}
-      guess = "elixirr"
-
       ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
-
-      ElixirWordle.MockWordleAPI
-      |> expect(:feedback, 1, fn guess, answer -> ElixirWordle.Wordle.feedback(guess, answer) end)
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
+      |> expect(:feedback, 1, fn guess, answer -> feedback_mock(guess, answer) end)
 
       assert {:ok, view, _html} = live(conn, "/")
 
-      # Submit a supported length guess, has to be displayed on the guesses board
+      # Submit a right length guess, which has to be displayed on the guesses board
+      guess = "gnalre"
       render_hook(view, :submit, %{guess: guess})
       assert view |> element("#guesses-board") |> render() =~ guess
 
@@ -97,100 +92,117 @@ defmodule ElixirWordleWeb.WordleLiveTest do
       refute view |> element("#inputs-board") |> render() =~ "input-6-"
     end
 
-    test "when the guess has less length than the answer, a message will be displayed", %{
-      conn: conn
-    } do
-      mock_data = %{answer: "elixirw", clue: "This is the clue", description: ""}
-      guess = "el"
-
+    test "when the guess has less length than the answer, a message will be displayed",
+         %{conn: conn} do
       ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
 
       assert {:ok, view, _html} = live(conn, "/")
 
-      # Submit guess, which has less length than the answer, display a message
+      # Submit a guess which has less length than the answer, it has to display a warning message
+      guess = "el"
       render_hook(view, :submit, %{guess: guess})
-      assert view |> element("#warningMessage") |> render() =~ "Not enough letters"
       refute view |> element("#guesses-board") |> render() =~ guess
+      assert view |> element("#warningMessage") |> render() =~ "Not enough letters"
     end
 
-    test "when the guess contains more letters than the answer, a message will be displayed", %{
-      conn: conn
-    } do
-      mock_data = %{answer: "elixirw", clue: "This is the clue", description: ""}
-      guess = "elixirwww"
-
+    test "when the guess contains more letters than the answer, a message will be displayed",
+         %{conn: conn} do
       ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
 
       assert {:ok, view, _html} = live(conn, "/")
 
-      # Submit guess, which has less length than the answer, display a message
+      # Submit a guess which has more length than the answer, it has to display a warning message
+      guess = "erlangss"
       render_hook(view, :submit, %{guess: guess})
       assert view |> element("#warningMessage") |> render() =~ "Too many letters"
       refute view |> element("#guesses-board") |> render() =~ guess
     end
   end
 
-  describe "directly testable scenarios derived from when there aren't more attempts" do
-    test "when you accurately guess the word, there's no more attempts", %{conn: conn} do
-      mock_data = %{answer: "elixirw", clue: "This is the clue", description: ""}
-
+  describe "submit (when there aren't more attempts)" do
+    test "when you accurately guess the word, then there's no more attempts", %{conn: conn} do
       ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
-
-      ElixirWordle.MockWordleAPI
-      |> expect(:feedback, 1, fn guess, answer -> ElixirWordle.Wordle.feedback(guess, answer) end)
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
+      |> expect(:feedback, 1, fn guess, answer -> feedback_mock(guess, answer) end)
 
       assert {:ok, view, _html} = live(conn, "/")
+      render_hook(view, :submit, %{guess: @generic_data.answer})
 
-      render_hook(view, :submit, %{guess: mock_data.answer})
-
-      # The word set at page & the congratulation message
-      assert view |> element("#guesses-board") |> render() =~ mock_data.answer
+      # The word is set at guesses-board & the congratulation message is displayed
+      assert view |> element("#guesses-board") |> render() =~ @generic_data.answer
       assert view |> element("#warningMessage") |> render() =~ "You won !"
 
-      # No inputs' tiles
+      # There are no more inputs' tiles
       refute view |> element("#inputs-board") |> render() =~ "input-1-1"
 
-      # Attempting to submit another guess when there are no more attempts available
-      no_more_attempts_guess = "abcdefg"
-      render_hook(view, :submit, %{guess: no_more_attempts_guess})
+      # Attempting to submit another guess when there are no more attempts, it display a warning message
+      word = "rtasmh"
+      render_hook(view, :submit, %{guess: word})
       assert view |> element("#warningMessage") |> render() =~ "There aren&#39;t more attempts"
-      refute view |> element("#guesses-board") |> render() =~ no_more_attempts_guess
+      refute view |> element("#guesses-board") |> render() =~ word
     end
 
     test "when you exhaust the max number of attempts, a message will be displayed", %{conn: conn} do
-      max_attempts = 6
-
-      mock_data = %{answer: "example", clue: "This is the clue", description: "blah blah"}
-      guess = "abcdefg"
-
       ElixirWordle.MockWordleAPI
-      |> expect(:get_word_info, 2, fn -> {:ok, mock_data} end)
-
-      ElixirWordle.MockWordleAPI
-      |> expect(:feedback, max_attempts, fn guess, answer ->
-        ElixirWordle.Wordle.feedback(guess, answer)
-      end)
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
+      |> expect(:feedback, @max_attempts, fn guess, answer -> feedback_mock(guess, answer) end)
 
       assert {:ok, view, _html} = live(conn, "/")
 
-      # catch the last attempt
-      for _i <- 1..max_attempts, do: render_hook(view, :submit, %{guess: guess})
+      # Exhaust attempts
+      for _i <- 1..@max_attempts, do: render_hook(view, :submit, %{guess: "gnalre"})
 
-      # exhaust attempts & the lost message
+      # Display the lost message
       assert view |> element("#warningMessage") |> render() =~ "You lost !"
-      refute view |> element("#guesses-board") |> render() =~ mock_data.answer
 
-      # No inputs' tiles
+      # There are no more inputs' tiles
       refute view |> element("#inputs-board") |> render() =~ "input-1-1"
 
-      # Attempting to submit another guess when there are no more attempts available
-      other_guess = "gfedcba"
-      render_hook(view, :submit, %{guess: other_guess})
+      # Attempting to submit another guess when there are no more attempts, it display a warning message
+      word = "irling"
+      render_hook(view, :submit, %{guess: word})
       assert view |> element("#warningMessage") |> render() =~ "There aren&#39;t more attempts"
-      refute view |> element("#guesses-board") |> render() =~ other_guess
+      refute view |> element("#guesses-board") |> render() =~ word
     end
+  end
+
+  describe "wordle ending" do
+    @tag :slow_test
+    test "when ends (no more attempts), it's  displayed the result button", %{conn: conn} do
+      ElixirWordle.MockWordleAPI
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
+      |> expect(:feedback, 1, fn guess, answer -> feedback_mock(guess, answer) end)
+
+      assert {:ok, view, _html} = live(conn, "/")
+      render_hook(view, :submit, %{guess: @generic_data.answer})
+
+      # Result button is render after 1'7s
+      :timer.sleep(1800)
+      assert view |> has_element?("#button-results")
+    end
+
+    test "when handle_info/2, it's  displayed the result button", %{conn: conn} do
+      ElixirWordle.MockWordleAPI
+      |> expect(:get_word_info, 2, fn -> {:ok, @generic_data} end)
+      |> expect(:feedback, 1, fn guess, answer -> feedback_mock(guess, answer) end)
+
+      assert {:ok, view, _html} = live(conn, "/")
+      render_hook(view, :submit, %{guess: @generic_data.answer})
+
+      Process.send(view.pid, :ends, [])
+      assert view |> has_element?("#button-results")
+    end
+  end
+
+  defp feedback_mock(guess, answer) do
+    result = (guess == answer && :match) || :letter_match
+
+    {:ok,
+     %{
+       guess: guess,
+       feedback: for(_i <- 1..String.length(answer), do: result)
+     }}
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,7 @@ Mox.defmock(ElixirWordle.WordleMock, for: ElixirWordle.WordleAPI)
 Application.put_env(:elixir_wordle, :wordle_api, ElixirWordle.WordleMock)
 
 ExUnit.start()
-ExUnit.configure(exclude: :wallaby)
+ExUnit.configure(exclude: [:wallaby, :slow_test])
 
 # Wallaby
 {:ok, _} = Application.ensure_all_started(:wallaby)
