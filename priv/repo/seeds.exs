4af9ae44e0978d6bcb925c0b2ac9807a2b67455b
Icia Carro <iciacarrobarallobre@gmail.com>
2023-08-23 15:31:39 +0200

    deploy & docu

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,6 +2,10 @@
 # You Can Run It As:
 #
 # mix run priv/repo/seeds.exs
+#
+# Run in fly.io:
+# fly ssh console --pty -C "/app/bin/elixir_wordle remote"
+# ElixirWordle.Seed.seeding()
 
 alias ElixirWordle.Seed
 
